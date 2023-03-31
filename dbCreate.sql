e72c5663224f45ef1eb4d8b571f84971f171c33f
David Cristóbal Valero Croma <david.valero@usach.cl>
2023-03-30 23:21:33 -0300

    :0

---
+++ dbCreate.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE "Colegio"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+COMMENT ON DATABASE "Colegio"
+    IS 'Control 1 de Tbd';
